4d2fecf374342986338b7577b3066150be1a5a13
Maowei Jiang <97515411+lemonade1258@users.noreply.github.com>
2026-03-20 18:49:04 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  // 把 "/" 改成了你的仓库名，注意前后都要有斜杠
+  base: "/clain-whu.github.io/", 
   build: {
     outDir: "dist",
     assetsDir: "assets",
